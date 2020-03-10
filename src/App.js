2322e008db1b45d561db8d7f9a7488eeb1fd9bc5
Andychuang <35476023+AndyChuang1@users.noreply.github.com>
2020-03-10 22:13:37 +0800

    update card list render smaple

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.scss';
+import Counter from './components/counter';
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <Counter></Counter>
+      </header>
+    </div>
+  );
+}
+
+export default App;
