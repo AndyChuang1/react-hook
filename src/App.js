72344293587e05b08f660f5d94317b0394859a66
Andychuang <35476023+AndyChuang1@users.noreply.github.com>
2020-03-28 14:46:20 +0800

    add bootstrap and adjust style

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import './App.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Counter from './components/CounterClass';
-import CounterS from './components/CounterStyle';
-import CounterR from './components/CounterRender';
+import Cards from './components/Card';
+
 function App() {
   return (
     <div className='App'>
       <header className='App-header'>
         <Counter></Counter>
+        <Cards
+          header='KFC'
+          title='Fried Chicken'
+          text='Special recipt in our fried Chieken'
+        ></Cards>
         {/* <CounterS></CounterS> */}
         {/* <CounterR></CounterR> */}
       </header>
--- src/components/Card.js
+++ src/components/Card.js
@@ -3,8 +3,8 @@ import React from 'react';
 import { Card } from 'react-bootstrap';
 function Cards({ header, title, text }) {
   return (
-    <div className='counter'>
-      <Card className='mr-2' bg='light' style={{ width: '18rem' }}>
+    <div className='Cards'>
+      <Card className='mr-2' bg='dark' style={{ width: '18rem' }}>
         <Card.Header>{header}</Card.Header>
         <Card.Body>
           <Card.Title>{title}</Card.Title>
